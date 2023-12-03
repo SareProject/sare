7c490c616070d3bfe77760f420dae27a4c952928
Zola Gonano <zolagonano@protonmail.com>
2023-12-03 14:11:53 +0000

    Implement BSON encoding for MetadataFormat

--- cryptopia-core/src/format/mod.rs
+++ cryptopia-core/src/format/mod.rs
@@ -1,3 +1,4 @@
+use bson::{bson, Bson};
 use serde::{Deserialize, Serialize};
 
 use crate::hybrid_kem::{DHAlgorithm, KEMAlgorithm};
@@ -6,7 +7,9 @@ use crate::kdf::{HKDFAlgorithm, PKDFAlgorithm};
 
 //TODO: Define in encryption module
 #[derive(Serialize, Deserialize)]
-pub struct EncryptionAlgorithm();
+pub enum EncryptionAlgorithm {
+    AES256GCM,
+}
 
 const MAGIC_BYTES: &[u8; 9] = b"CRYPTOPIA";
 
@@ -29,12 +32,24 @@ pub struct EncryptionMetadataFormat {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct PKDFMetadataFormat {
+    pkdf_algorithm: PKDFAlgorithm,
+    pkdf_workfactor_scale: u32,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct MetadataFormat {
-    kem_metadata: KEMMetadataFormat,
+    kem_metadata: Option<KEMMetadataFormat>,
     signature_metadata: Option<SignatureMetadataFormat>,
     encryption_metadata: EncryptionMetadataFormat,
-    pkdf_algorithm: Option<PKDFAlgorithm>,
+    pkdf_metadata: Option<PKDFMetadataFormat>,
     comment: Option<String>,
+}
+
+impl MetadataFormat {
+    pub fn encode(&self) -> Vec<u8> {
+        bson::to_vec(&self).unwrap()
+    }
 }
 
 pub struct HeaderFormat {
@@ -43,4 +58,33 @@ pub struct HeaderFormat {
     metadata: Vec<u8>,
     signature_length: u32,
     signature: Vec<u8>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const ENCODED_METADATA: &str = "zQAAAAprZW1fbWV0YWRhdGEACnNpZ25hdHVyZV9tZXRhZGF0YQADZW5jcnlwdGlvbl9tZXRhZGF0YQApAAAAAmVuY3J5cHRpb25fYWxnb3JpdGhtAAoAAABBRVMyNTZHQ00AAANwa2RmX21ldGFkYXRhAD8AAAACcGtkZl9hbGdvcml0aG0ABwAAAFNjcnlwdAAScGtkZl93b3JrZmFjdG9yX3NjYWxlADIAAAAAAAAAAAJjb21tZW50AA0AAABUZXN0IENvbW1lbnQAAA==";
+
+    #[test]
+    fn metadata_format_encode() {
+        let encryption_metadata = EncryptionMetadataFormat {
+            encryption_algorithm: EncryptionAlgorithm::AES256GCM,
+        };
+
+        let pkdf_metadata = PKDFMetadataFormat {
+            pkdf_algorithm: PKDFAlgorithm::Scrypt,
+            pkdf_workfactor_scale: 50,
+        };
+
+        let metadata = MetadataFormat {
+            kem_metadata: None,
+            signature_metadata: None,
+            encryption_metadata,
+            pkdf_metadata: Some(pkdf_metadata),
+            comment: Some("Test Comment".to_string()),
+        };
+
+        assert_eq!(ENCODED_METADATA, &base64::encode(metadata.encode()));
+    }
 }
