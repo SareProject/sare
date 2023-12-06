91de2646df8e6449d0b605b555831a7e070763e3
Zola Gonano <zolagonano@protonmail.com>
2023-12-06 19:01:39 +0000

    Manually Serialize/Deserialize the SecretVec type

--- cryptopia-core/src/format/keys.rs
+++ cryptopia-core/src/format/keys.rs
@@ -33,7 +33,28 @@ pub struct SecretKeyFormat {
     pq_algorithm: PQAlgorithm,
     dh_algorithm: DHAlgorithm,
     kem_algorithm: KEMAlgorithm,
+    #[serde(with = "secret_vec_serde")]
     master_seed: SecretVec<u8>,
     #[serde(skip_serializing_if = "Option::is_none", flatten)]
     encryption_metadata: Option<EncryptionMetadataFormat>,
+}
+
+mod secret_vec_serde {
+    use secrecy::{ExposeSecret, SecretVec};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(data: &SecretVec<u8>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_bytes(&data.expose_secret())
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<SecretVec<u8>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let bytes: Vec<u8> = Vec::deserialize(deserializer)?;
+        Ok(SecretVec::new(bytes))
+    }
 }
