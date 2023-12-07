80d24684844e1b6d5080c0a7c20fda99862622bb
Zola Gonano <zolagonano@protonmail.com>
2023-12-07 18:37:10 +0000

    Implement encode/decode for SecretKeyFromat

--- cryptopia-core/src/format/keys.rs
+++ cryptopia-core/src/format/keys.rs
@@ -1,7 +1,8 @@
-use secrecy::SecretVec;
+use secrecy::{ExposeSecret, SecretVec};
 use serde::{Deserialize, Serialize};
 
 use crate::format::encryption::EncryptionMetadataFormat;
+use crate::format::FormatError;
 use crate::hybrid_kem::{DHAlgorithm, KEMAlgorithm};
 use crate::hybrid_sign::{ECAlgorithm, PQAlgorithm};
 
@@ -37,6 +38,19 @@ pub struct SecretKeyFormat {
     master_seed: SecretVec<u8>,
     #[serde(skip_serializing_if = "Option::is_none", flatten)]
     encryption_metadata: Option<EncryptionMetadataFormat>,
+}
+
+impl SecretKeyFormat {
+    pub fn encode(&self) -> SecretVec<u8> {
+        SecretVec::from(bson::to_vec(&self).unwrap())
+    }
+
+    pub fn decode(bson_secretkey: &SecretVec<u8>) -> Result<Self, FormatError> {
+        let secret_key = bson::from_slice::<SecretKeyFormat>(bson_secretkey.expose_secret());
+
+        // TODO: Needs Error Handling
+        Ok(secret_key.unwrap())
+    }
 }
 
 mod secret_vec_serde {
