f4b53037914d53255964b71319abf703177eb346
Zola Gonano <zolagonano@protonmail.com>
2023-12-14 14:05:19 +0000

    Implement xchacha20poly1305 encrypt/decrypt methods to Encryptor and Decryptor structs

--- sare-core/src/encryption/mod.rs
+++ sare-core/src/encryption/mod.rs
@@ -1,12 +1,19 @@
 use serde::{Deserialize, Serialize};
 
+use aead::stream;
 use aes_kw::KekAes256;
+use chacha20poly1305::KeyInit;
+use chacha20poly1305::XChaCha20Poly1305;
 use secrecy::{ExposeSecret, SecretVec};
+use std::io::{Read, Write};
+
+const AEAD_BUFFER_LEN: usize = 2048;
 
 #[derive(Serialize, Deserialize)]
 pub enum EncryptionAlgorithm {
     AES256GCM,
     AES256KW,
+    XCHACHA20POLY1305,
 }
 
 #[derive(Debug)]
@@ -38,5 +45,116 @@ impl KeyWrap {
         kek.wrap(data.expose_secret(), &mut output);
 
         output
+    }
+}
+
+pub struct Encryptor {
+    input_key: SecretVec<u8>,
+    nonce: Vec<u8>,
+    algorithm: EncryptionAlgorithm,
+}
+
+impl Encryptor {
+    pub fn new(input_key: SecretVec<u8>, nonce: Vec<u8>, algorithm: EncryptionAlgorithm) -> Self {
+        Encryptor {
+            input_key,
+            nonce,
+            algorithm,
+        }
+    }
+
+    pub fn encrypt<R: Read, W: Write>(&self, data: R, output: W) -> Result<(), EncryptionError> {
+        todo!();
+    }
+
+    // TODO: Needs Error Handling
+    pub fn encrypt_xchacha20poly1305<R: Read, W: Write>(
+        &self,
+        mut data: R,
+        mut output: W,
+    ) -> Result<(), EncryptionError> {
+        let input_key = <[u8; 32]>::try_from(self.input_key.expose_secret().as_slice()).unwrap();
+        let nonce = <[u8; 19]>::try_from(self.nonce.as_slice()).unwrap();
+
+        let aead = XChaCha20Poly1305::new(input_key.as_ref().into());
+        let mut stream_aead = stream::EncryptorBE32::from_aead(aead, nonce.as_ref().into());
+        let mut data_buffer = [0u8; AEAD_BUFFER_LEN];
+
+        loop {
+            let read_count = data.read(&mut data_buffer).unwrap();
+
+            if read_count == AEAD_BUFFER_LEN {
+                let encrypted_data = stream_aead.encrypt_next(data_buffer.as_slice()).unwrap();
+
+                output.write(&encrypted_data).unwrap();
+            } else {
+                let encrypted_data = stream_aead
+                    .encrypt_last(&data_buffer[..read_count])
+                    .unwrap();
+
+                output.write(&encrypted_data).unwrap();
+                break;
+            }
+        }
+        Ok(())
+    }
+}
+
+pub struct Decryptor {
+    input_key: SecretVec<u8>,
+    nonce: Vec<u8>,
+    algorithm: EncryptionAlgorithm,
+}
+
+impl Decryptor {
+    pub fn new(input_key: SecretVec<u8>, nonce: Vec<u8>, algorithm: EncryptionAlgorithm) -> Self {
+        Decryptor {
+            input_key,
+            nonce,
+            algorithm,
+        }
+    }
+
+    pub fn decrypt<R: Read, W: Write>(
+        &self,
+        encrypted_data: R,
+        output: W,
+    ) -> Result<(), EncryptionError> {
+        todo!();
+    }
+
+    // TODO: Needs Error Handling
+    pub fn decrypt_xchacha20poly1305<R: Read, W: Write>(
+        &self,
+        mut encrypted_data: R,
+        mut output: W,
+    ) -> Result<(), EncryptionError> {
+        let input_key = <[u8; 32]>::try_from(self.input_key.expose_secret().as_slice()).unwrap();
+        let nonce = <[u8; 19]>::try_from(self.nonce.as_slice()).unwrap();
+
+        let aead = XChaCha20Poly1305::new(input_key.as_ref().into());
+        let mut stream_aead = stream::DecryptorBE32::from_aead(aead, nonce.as_ref().into());
+        let mut encrypted_buffer = [0u8; AEAD_BUFFER_LEN + 16]; // 16bytes for AEAD tag
+
+        loop {
+            let read_count = encrypted_data.read(&mut encrypted_buffer).unwrap();
+
+            if read_count == AEAD_BUFFER_LEN {
+                let decrypted_data = stream_aead
+                    .decrypt_next(encrypted_buffer.as_slice())
+                    .unwrap();
+
+                output.write(&decrypted_data).unwrap();
+            } else {
+                let decrypted_data = stream_aead
+                    .decrypt_last(&encrypted_buffer[..read_count])
+                    .unwrap();
+
+                output.write(&decrypted_data).unwrap();
+                break;
+            }
+        }
+
+        Ok(())
     }
 }
