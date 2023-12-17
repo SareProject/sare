2b1a243892381d1d9a1685acbed162f2bddfcba8
Zola Gonano <zolagonano@protonmail.com>
2023-12-17 12:58:43 +0000

    Add a CLI template using argh for implementing subcommands later on

--- sare-cli/src/main.rs
+++ sare-cli/src/main.rs
@@ -1,3 +1,44 @@
+use argh::FromArgs;
+
+use std::fs::File;
+use std::path::PathBuf;
+
+use sare_lib::keys;
+
+#[derive(FromArgs)]
+/// Safe At Rest Encryption. A tool to stay Safe in the Quantum Age
+struct SareCli {
+    #[argh(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand)]
+enum SubCommand {
+    KeyGen(KeyGen),
+}
+
+#[derive(FromArgs)]
+/// Generates a SARE keypair
+#[argh(subcommand, name = "keygen")]
+struct KeyGen {}
+
+fn generate_key_pair() -> Result<(), String> {
+    todo!();
+    //let output_file = File::create(output_path);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse command-line arguments
+    let args: SareCli = argh::from_env();
+
+    match args.cmd {
+        SubCommand::KeyGen(_) => {
+            if let Err(err) = generate_key_pair() {
+                eprintln!("Error: {}", err);
+            } else {
+                println!("Key pair generated!");
+            }
+        }
+    }
 }
