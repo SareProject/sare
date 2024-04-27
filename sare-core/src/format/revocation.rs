2dd5211665f0ecca8f07c5d4ff8c08f9490bc12d
Zola Gonano <zolagonano@protonmail.com>
2024-04-27 14:24:23 +0000

    Update structure of Certificate formats in sare-core

--- sare-core/src/format/revocation.rs
+++ sare-core/src/format/revocation.rs
@@ -1,54 +1,39 @@
 use serde::{Deserialize, Serialize};
-use sha2::Digest;
-
 use super::{EncodablePublic, FormatError};
 
-const REVOCATION_CERTIFICATE_PEM_TAG: &str = "SARE REVOCATION CERTIFICATE";
+const CERTIFICATE_PEM_TAG: &str = "SARE CERTIFICATE";
 
-#[derive(Serialize, Deserialize)]
-pub enum FingerprintAlgo {
-    SHA2_256,
-    SHA3_256,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Fingerprint {
-    algorithm: FingerprintAlgo,
-    fingerprint_hash: Vec<u8>,
-}
-
-impl Fingerprint {
-    pub fn from_hash(fingerprint_hash: &[u8], algorithm: FingerprintAlgo) -> Self {
-        Fingerprint {
-            algorithm,
-            fingerprint_hash: fingerprint_hash.to_vec(),
-        }
-    }
-}
-
-// TODO: this format needs to be replaced with Certificate format, to allow more flexibility
-// in the future
 #[derive(Serialize, Deserialize)]
 pub struct RevocationCertificateFormat {
-    pub name: Option<String>,
-    pub email: Option<String>,
-    pub expiry_date: Option<i64>,
-    pub fingerprint: Fingerprint, // SHA Hash of public keys
+    revocation_date: Option<i64>,
+    revocation_reason: String,
 }
 
-impl EncodablePublic for RevocationCertificateFormat {
+#[derive(Serialize, Deserialize)]
+pub enum CertificateType {
+    Revocation(RevocationCertificateFormat),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CertificateFormat {
+    issuer: String,
+    expiry_date: Option<i64>,
+    certificate_type: CertificateType
+}
+
+impl EncodablePublic for CertificateFormat {
     fn encode_bson(&self) -> Vec<u8> {
         bson::to_vec(&self).unwrap()
     }
 
     fn decode_bson(bson_data: &[u8]) -> Result<Self, FormatError> {
-        let public_key = bson::from_slice::<RevocationCertificateFormat>(bson_data).unwrap();
+        let public_key = bson::from_slice::<CertificateFormat>(bson_data).unwrap();
         Ok(public_key)
     }
 
     fn encode_pem(&self) -> String {
         let pem = pem::Pem::new(
-            REVOCATION_CERTIFICATE_PEM_TAG,
+            CERTIFICATE_PEM_TAG,
             self.encode_bson().as_slice(),
         );
         pem::encode(&pem)
