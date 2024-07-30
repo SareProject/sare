99074cf9f82f77f68c05b263610c506b88ca6e1f
Zola Gonano <zolagonano@protonmail.com>
2024-07-30 11:42:00 +0000

    Implement read_cli_secret

---
+++ sare-cli/src/lib/common.rs
@@ -0,0 +1,9 @@
+use rpassword;
+use secrecy::SecretString;
+
+// TODO: Return SareCLIError Instead Of String
+pub fn read_cli_secret(prompt: impl ToString) -> Result<SecretString, String>{
+    let secret: SecretString = rpassword::prompt_password(prompt).map_err(|e| e.to_string())?.into();
+
+    Ok(secret)
+}
