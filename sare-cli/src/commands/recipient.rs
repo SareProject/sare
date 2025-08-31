3b731261633b5999160570a3a97b813ec20ed123
Zola Gonano <zolagonano@protonmail.com>
2025-08-31 09:54:43 +0000

    Implement add_recipient command

--- sare-cli/src/commands/recipient.rs
+++ sare-cli/src/commands/recipient.rs
@@ -1,7 +1,7 @@
 use std::{
     fs::{self, File},
     io::Cursor,
-    path::Path,
+    path::{Path, PathBuf},
     time::Duration,
 };
 
@@ -9,15 +9,14 @@ use argh::FromArgs;
 
 use indicatif::ProgressBar;
 use sare_lib::keys::{
-    FullChainPublicKeyFormat, HybridKEMAlgorithm, HybridSignAlgorithm, MasterKey,
-    RecipientPublicKey,
+    FullChainPublicKeyFormat, HybridKEMAlgorithm, HybridSignAlgorithm, MasterKey, SharedPublicKey,
 };
 use secrecy::{ExposeSecret, SecretVec};
 
 use crate::{
-    commands::revocation::RevocationCommand,
-    common,
-    db::{self, SareDB},
+    commands::{recipient, revocation::RevocationCommand},
+    common::{self, prepare_sare_directory},
+    db::{self, SareDB, SareDBRecipient},
     SareCLIError,
 };
 
@@ -34,7 +33,7 @@ enum RecipientSubCommand {
 /// Add a new recipient
 struct AddRecipient {
     #[argh(positional)]
-    key: String,
+    key: PathBuf,
 }
 
 #[derive(FromArgs, Debug)]
@@ -62,14 +61,67 @@ pub struct RecipientCommand {
 
 impl RecipientCommand {
     pub fn execute(&self) -> Result<(), SareCLIError> {
+        match &self.sub {
+            RecipientSubCommand::Add(add) => self.add_recipient(add)?,
+            RecipientSubCommand::Remove(remove) => self.remove_recipient(remove)?,
+            RecipientSubCommand::List(_) => Self::list_recipients()?,
+        }
         Ok(())
     }
 
     fn add_recipient(&self, add: &AddRecipient) -> Result<(), SareCLIError> {
         let pem_content = fs::read_to_string(&add.key)?;
-        let recipient_key = RecipientPublicKey::from_pem(pem_content)?;
+        let recipient_key = SharedPublicKey::from_pem(pem_content)?;
+        let fullchain_fingerprint = recipient_key.fullchain_public_key.calculate_fingerprint();
 
-        todo!();
+        let sare_directory = common::prepare_sare_directory()?;
+
+        let fullchain_fingerprint = hex::encode_upper(fullchain_fingerprint);
+
+        // Creating temp directories
+        let temp_dir = sare_directory.join(".temp");
+        fs::create_dir_all(&temp_dir.join("recipients"))?;
+
+        let mut recipient_buffer = Cursor::new(Vec::new());
+        recipient_key.export(recipient_buffer.get_mut())?;
+
+        // Export to temp dirs
+        let recipient_path_temp = temp_dir
+            .join("recipients")
+            .join(format!("RECIPIENT_{}.pem", &fullchain_fingerprint.as_str()));
+        fs::write(&recipient_path_temp, &recipient_buffer.into_inner())?;
+
+        let is_key_verified =
+            if let Some(verification_certificate) = recipient_key.validation_certificate {
+                verification_certificate.verify()?
+            } else {
+                false
+            };
+
+        let recipient_final = sare_directory
+            .join("recipients")
+            .join(format!("RECIPIENT_{}.pem", &fullchain_fingerprint));
+
+        fs::rename(recipient_path_temp, recipient_final)?;
+
+        let comment = common::get_confirmed_input(
+            "Add any comment or aditional information for this recipient: ",
+        );
+
+        let recipient = SareDBRecipient::new(&fullchain_fingerprint, Some(comment));
+
+        let mut sare_db = SareDB::import_from_json_file()?;
+        sare_db.add_recipient(recipient);
+        sare_db.save_to_json_file()?;
+
+        println!(
+            "\nPublicKey added as recipient
+        \n\tLOCATION: {:?},
+        \n\tPUB: {}\n\tVERIFIED: {}",
+            sare_directory.join("recipients"), fullchain_fingerprint, is_key_verified
+        );
+
+        Ok(())
     }
 
     fn remove_recipient(&self, remove: &RemoveRecipient) -> Result<(), SareCLIError> {
